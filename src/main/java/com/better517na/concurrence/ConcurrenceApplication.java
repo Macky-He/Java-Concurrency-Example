f61bc5ed88f0c3253841c6e06650c2894294df96
Macky-He <97462319@qq.com>
2019-02-28 23:10:11 +0800

    init project

---
+++ src/main/java/com/better517na/concurrence/ConcurrenceApplication.java
@@ -0,0 +1,12 @@
+package com.better517na.concurrence;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConcurrenceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ConcurrenceApplication.class, args);
+    }
+}
